4262c1d19311e83a21516b16ecd5589f0f86c493
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-03-23 00:41:05 +0500

    Add files via upload

---
+++ Lab11/11LabSpiski2/laba11b.cpp
@@ -0,0 +1,86 @@
+﻿#include<iostream>
+using namespace std;
+struct List {
+	int data;	
+	List* next=NULL;
+	List* prev=NULL;
+};
+List* make(int size);
+void GetList(List* first);
+void Deletion(List* &first);
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int size = 0;
+	while (size <1) {
+		cout << "Введите размер списка: ";
+		cin >> size;
+	}
+	List* list = make(size);
+	cout << "Ваш список: " << " ";	
+	GetList(list);
+	if (size > 1) {
+		cout << "Удаление первого элемента с чётным полем..." << endl;
+		Deletion(list);
+		GetList(list);
+	}
+}
+List* make(int size)
+{
+	if (size == 0) {
+		cout << "Список пуст!" << endl;
+		return 0;
+	}	
+	List* head = new List;
+	cout << "Введите 1 элемент: ";
+	List* p = head;
+	cin >> head->data;
+	for (int i = 2; i <= size; i++) {
+		List* body = new List;
+		p->next = body;
+		cout << "Введите " << i << " элемент: ";
+		cin >> body->data;
+		body->prev = p;
+		p = body;
+	}
+	return head;	
+}
+void GetList(List* first)
+{
+	if (first == NULL)
+		cout << "Список пуст!" << endl;
+	else {
+		List* p = first;
+		while (p != NULL) {
+			cout << p->data << " ";
+			p = p->next;
+			}
+		cout << endl;
+	}
+}
+void Deletion(List*& first)
+{	
+	if (first == NULL)
+		cout << "Список пуст!" << endl;
+	else {
+		List* p = first;
+		int i = 1;
+		bool flag = true;
+		while (p != NULL && flag) {
+			if (i == 2) {
+				List* thiz = p;
+				if (p->prev) {
+					p->prev->next = thiz->next;
+					p->next->prev = p->prev;
+				}
+				else {
+					first = p->next;
+				}
+				delete thiz;
+				flag = false;				
+			}
+			p = p->next;			
+			i++;
+		}		
+	}	
+}
---
+++ Lab11/Lab11spiski/laba11.cpp
@@ -0,0 +1,78 @@
+﻿#include<iostream>
+using namespace std;
+struct List {
+	char data;
+	List* next;	
+};
+void GetList(List* first);
+List* make(int size);
+List* make2(List* first);
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int size = 0;
+	while (size < 1) {
+		cout << "Введите размер списка: ";
+		cin >> size;
+	}
+	List* list = make(size);
+	cout << "Ваш список: " << endl;
+	GetList(list);
+	make2(list);
+	GetList(list);
+}
+List* make(int size)
+{
+	if (size == 0) {
+		cout << "Список пуст!" << endl;
+		return 0;
+	}
+	List* first, * p;
+	first = NULL;
+	cout << "Введите 1 элемент: ";
+	p = new List;
+	cin >> p->data;
+	first = p;
+	for (int i = 2; i <= size; i++) {
+		List* nex = new List;
+		p->next = nex;
+		p = p->next;
+		cout << "Введите " << i << " элемент: ";
+		cin >> p->data;
+		p->next = NULL;
+	}
+	return first;
+}
+void GetList(List* first)
+{
+	if (first == NULL)
+		cout << "Список пуст!" << endl;
+	else {
+		List* p = first;
+		while (p!=NULL) {
+			cout << p->data << " ";
+			p = p->next;
+		}
+		cout << endl;
+	}
+}
+List* make2(List* first)
+{
+	char x;
+	cout << "Введите элемент для вставки: ";
+	cin >> x;
+	int i = 1;
+	List* p = first;
+	while (p!=NULL) {
+		if (i % 2 != 0) {
+			List* old = p->next;
+			List* newElement = new List;			
+			newElement->data=x;
+			newElement->next = old;
+			p->next = newElement;			
+		}
+		i++;
+		p = p->next;
+	}
+	return first;
+}
