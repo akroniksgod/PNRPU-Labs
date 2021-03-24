7b6f5c793968b8b1e268a031b08b373a9e1264cd
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-03-25 00:07:47 +0500

    Add files via upload

---
+++ lab19/Stack.cpp
@@ -0,0 +1,68 @@
+#ifndef _STACK_CPP_
+#define _STACK_CPP_
+#include "Stack.h"
+#include <assert.h>
+template<class Type>
+Stack<Type>::Stack()
+{
+}
+template<class Type>
+Stack<Type>::Stack(int s, Type element)
+{
+	size = s;
+	root = new Type[size];
+	for (int i = 0; i < size; i++) {
+		root[i] = rand() % 100;
+	}
+}
+template<class Type>
+Stack<Type>::~Stack()
+{
+	delete[] root;
+}
+template<class Type>
+void Stack<Type>::push(Type& newElement)
+{
+	Type* temp = new Type[size + 1];
+	for (int i = 0; i < size; i++) {
+		temp[i] = root[i];
+	}
+	temp[size++] = newElement;
+	root = temp;
+	temp = NULL;
+}
+template<class Type>
+void Stack<Type>::pop()
+{
+	Type* temp = new Type[size - 1];
+	for (int i = 0; i < size - 1; i++) {
+		temp[i] = root[i];
+	}
+	size--;
+	delete[] root;
+	root = temp;
+	temp = NULL;
+	if (size == 0) root = NULL;
+}
+template<class Type>
+bool Stack<Type>::empty()
+{
+	if (root == NULL)
+		return true;
+	else
+		return false;
+}
+template<class Type>
+void Stack<Type>::getStack()
+{
+	cout << endl;
+	for (int i = size - 1; i >= 0; i--) {
+		cout << root[i] << "; ";
+	}
+}
+template<class Type>
+int Stack<Type>::get_size()
+{
+	return size;
+}
+#endif
