d6c8d7ae37c027d35fcb1a2a4ef8182f79057e48
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-12-12 00:52:23 +0500

    Added 2nd triangle

---
+++ 1st semester/Triangle2/Triangle2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int i, j, k, n, temp, temp1;
+	do
+	{
+		cout << "Введите основание треугольника (нечётное): ";
+		cin >> n;
+		cout << "\n";
+	} while (n % 2 == 0);
+
+	temp = n;
+	for (i = 1; i <= temp; i++)
+	{
+		temp1 = i;
+		for (j = 1; j <= n - (n / 2 + 1); j++)
+		{
+			cout << " ";
+		}
+		n = n - 2;
+		for (i = 1; i <= temp1; i++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
