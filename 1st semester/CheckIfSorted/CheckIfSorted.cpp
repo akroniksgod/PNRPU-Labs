47672404129ac12629a1a71b219a65ae83fa2550
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-12-12 01:13:31 +0500

    While loop exercise
    Looping until a smaller value is inputted

---
+++ 1st semester/CheckIfSorted/CheckIfSorted.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+//ОПРЕДЕЛИТЬ, ЯВЛЯЕТСЯ ЛИ СТРОКА ОТСОРТИРОВАННОЙ ПО ВОЗРОСТАНИЮ
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int a, b = 0;	
+	cin >> a;
+
+	bool flag = true;
+	if (a != 0)
+	{
+		cin >> b;
+		if (b != 0)
+		{
+			while ((b != 0) && (a != b) && flag)
+			{
+				flag = flag && (a < b);
+				if (flag)
+				{
+					a = b;
+					cin >> b;
+				}
+
+			}
+			if (flag)
+				cout << "Строка отсортирована\n";
+			else
+				cout << "Строка не отсортирована\n";
+		}
+		else
+			cout << "Последовательность не существует\n";
+	}
+	else
+		cout << "Последовательность не существует\n";
+
+}
