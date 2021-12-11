6e82d465fd29768f422bdd41206cad304e4f3d36
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-12-12 00:51:28 +0500

    Added 1st triangle

---
+++ 1st semester/ConsoleApplication1/Triangle1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+//Triangle
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int n, i, j, z;
+	cout << "Введите N: ";
+	cin >> n;
+	for (i = 1; i <= n + 1; i++)
+	{
+		z = i;
+		for (j = 1; j <= n - z + 1; j++)
+			cout << " ";
+
+		for (j = 1; j < z; j++)
+			cout << "*";
+
+		cout << endl;
+	}
+}
