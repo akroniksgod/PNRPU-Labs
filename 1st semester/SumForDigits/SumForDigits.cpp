f9168196ee4243fdc4f5b6fea7510c7dbe6ac83a
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-12-12 01:04:41 +0500

    Another pointless exercise

---
+++ 1st semester/SumForDigits/SumForDigits.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+
+	int N = 0, S = 0, x = 0;
+	while (N < 1)
+	{
+		cout << "Введите число:";
+		cin >> N;
+	}
+
+	x = N;
+	while (N > 0)
+	{
+		S += N % 10;
+		N = N / 10;
+	}
+	cout << "Введённое число: " << x << "; " << " Результат:" << S;
+}
