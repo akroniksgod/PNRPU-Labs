d7caed14337fe179a043ff3b8beb19be2021c200
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-12-12 01:23:15 +0500

    Another while loop exercise

---
+++ 1st semester/PositiveOrNegativeDigitFirst/PositiveOrNegativeDigitFirst.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream> 
+using namespace std;
+
+//РАНЬШЕ ПОЛОЖИТЕЛЬНОЕ ИЛИ ОТРИЦАТЕЛЬНОЕ
+int main()
+{
+	int n, i, a;
+	bool flag = false;
+	setlocale(LC_ALL, "Ru");
+	cout << "Введите кол-во элементов в последовательности: ";
+	cin >> n;
+	i = 1;
+	while (!flag && (i <= n))
+	{
+		cout << "Введите последовательность: ";
+		cin >> a;
+		if (a == 0)
+			i++;
+		else
+			flag = true;
+	}
+	if (flag)
+	{
+		if (a < 0)
+			cout << "Раньше встречается отрицательное число\n";
+		else
+			cout << "Раньше встречается положительное число\n";
+	}
+	else
+		cout << "Все элементы последовательности равны нулю\n";
+}
