3a7ce81219396c8525b9ac4d9af090a8867a3993
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-12-14 01:34:31 +0500

    Drawing a square with empty space inside

---
+++ 1st semester/EmptySquare/EmptySquare.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream> 
+using namespace std;
+
+//Пустой квадрат
+int main()
+{	
+	setlocale(LC_ALL, "Rus");
+	int n;
+	do
+	{
+		cout << "Введите сторону квадрата (больше 2): ";
+		cin >> n;
+	} while (n < 3);
+	
+	cout << endl;
+	for (int i = 1; i <= n; i++)
+		cout << "*" << " ";
+
+	cout << endl;
+
+	for (int i = 1; i < n - 1; i++)
+	{
+		cout << "*";
+
+		for (int j = 1; j <= n + (n - 3); j++)
+			cout << " ";
+
+		cout << "*" << endl;
+	}
+
+	for (int i = 1; i <= n; i++)
+		cout << "*" << " ";
+}
