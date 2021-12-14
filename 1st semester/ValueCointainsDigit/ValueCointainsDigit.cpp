5f526f0f1c3480b8c9675a5efb322ac90254d60f
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-12-15 00:08:35 +0500

    Pointless program imo

---
+++ 1st semester/ValueCointainsDigit/ValueCointainsDigit.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int N = 0, x = 0, z = 0;
+	bool foundDigit = false;
+	
+	do
+	{
+		cout << "Введите цифру Z: ";
+		cin >> z;
+	} while (z < -1 || z > 10);
+	
+	cout << "Введите число: ";
+	cin >> N;
+
+	if (N < 0)
+		N *= -1;
+
+	while (N > 0 && !foundDigit)
+	{
+		x = N % 10;
+		if (x == z)
+			foundDigit = true;
+		N /= 10;
+	}
+
+	if (foundDigit)
+		cout << "Такая цифра есть в числе\n";
+	else
+		cout << "Такой цифры нет в числе\n";
+}
