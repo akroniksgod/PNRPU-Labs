696375287ed4d0fd383dc5dff5b73592a8f22b3a
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-12-15 00:33:28 +0500

    OddDigitsForSelectedDigit

---
+++ 1st semester/OddDigitsForSelectedDigit/OddDigitsForSelectedDigit.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int c;
+	std::cout << "Введите цифру: ";
+	std::cin >> c;
+	switch (c)
+	{
+	case 0:
+	case 1:
+		std::cout << 1 << " ";
+	case 2:
+	case 3:
+		std::cout << 3 << " ";
+	case 4:
+	case 5:
+		std::cout << 5 << " ";
+	case 6:
+	case 7:
+		std::cout << 7 << " ";
+	case 8:
+	case 9:
+		std::cout << 9 << " ";
+		break;
+	default:
+		std::cout << "Введена не цифра\n";
+		break;
+	}
+}
