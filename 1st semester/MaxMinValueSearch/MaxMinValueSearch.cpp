d06e35902259d5fa1d48de3fc971c3a1143bccf3
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-12-15 00:27:50 +0500

    Trained searching for max and min values

---
+++ 1st semester/MaxMinValueSearch/MaxMinValueSearch.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream> 
+using namespace std;
+
+//ПОИСК МАКСИМУМА И МИНИМУМА
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int max, min, maxNumber = 1, sumMax = 1, count, a, minNumber = 1, sumMin = 1; 	
+	
+	cout << "Введите число N целых чисел: ";
+	cin >> count;
+	cout << "Введите число последовательности: ";
+	cin >> max;
+	
+	min = max;
+	for (int i = 2; i <= count; i++)
+	{
+		cout << "Введите число последовательности: ";
+		cin >> a;
+		if (a > max)
+		{
+			max = a;
+			maxNumber = i;
+			sumMax = 1;
+		}
+		else if (a == max)
+			sumMax++;
+
+		if (a < min)
+		{
+			min = a;
+			minNumber = i;
+			sumMin = 1;
+		}
+		else if (a == min)
+			sumMin++;
+	}
+
+	if (max == min)
+		cout << "\nВ множестве только одно число\n";
+	else
+	{
+		cout << "\nМаксимальное значение: " << max << endl << "Номер максимума: " << maxNumber << endl 
+			<< "Количество данных максимумов: " << sumMax << "\n";
+		cout << "\nМинимальное значение: " << min << endl << "Номер минимума: " << minNumber << endl 
+			<< "Количество данных минимумов: " << sumMin << "\n";
+	}
+}
