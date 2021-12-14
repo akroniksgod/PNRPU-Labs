2743f32bb382f7519c642719ccb70a19eabcfa44
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-12-15 00:43:21 +0500

    Used char type for "switch"

---
+++ 1st semester/CheckLettersFromWord/CheckLettersFromWord.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	char letter;
+	cout << "Введите букву: ";
+	cin >> letter;
+	switch (letter)
+	{
+	case 'и':
+		cout << "Буква и есть в слове\n";
+		break;
+	case 'н':
+		cout << "Буква н есть в слове\n";
+		break;
+	case 'ф':
+		cout << "Буква ф есть в слове\n";
+		break;
+	case 'о':
+		cout << "Буква о есть в слове\n";
+		break;
+	case 'р':
+		cout << "Буква р есть в слове\n";
+		break;
+	case 'м':
+		cout << "Буква м есть в слове\n";
+		break;
+	case 'а':
+		cout << "Буква а есть в слове\n";
+		break;
+	case 'т':
+		cout << "Буква т есть в слове\n";
+		break;
+	case 'к':
+		cout << "Буква к есть в слове\n";
+		break;
+	default:
+		cout << "Буквы " << letter << " нет в слове\n";
+		break;
+	}
+}
