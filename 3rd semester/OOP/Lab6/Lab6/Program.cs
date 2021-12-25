0682668ca097e8f4d90b1d02f3c00ed842291f05
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2021-12-26 00:19:21 +0500

    Added 6th lab in C#

---
+++ 3rd semester/OOP/Lab6/Lab6/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static bool IsVowel(char item)
+        {
+            string al = "eEyYuUiIoOaA";
+            for (int i = 0; i < al.Length; i++)
+            {
+                if (item == al[i])
+                    return true;
+            }
+            return false;
+        }
+        static bool ContainsCyrillicLetters(string toComp)
+        {
+            string al = "йцукенгшщзхъёфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ";
+            for (int i = 0; i < toComp.Length; i++)
+            {
+                for (int j = 0; j < al.Length; j++)
+                {
+                    if (toComp[i] == al[j])
+                        return true;
+                }                
+            }
+            return false;
+        }
+        static void CreateCharArrWithRandom(ref char[] arr)
+        {
+            string al = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = al[rnd.Next(0, al.Length - 1)];
+            }
+        }        
+        static void CreateCharArrManually(ref char[] arr)
+        {
+            Console.WriteLine($"Введите строку размером {arr.Length} латинскими символами");
+            bool isFine = false;
+            do
+            {
+                string temp = Console.ReadLine();
+                if (temp.Length <= arr.Length && !ContainsCyrillicLetters(temp))
+                {
+                    arr = temp.ToCharArray();
+                    isFine = true;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода!");
+                }
+            } while (!isFine);            
+            
+        }
+        static void RemoveLastVowelFrom(ref char[] arr)
+        {
+            char[] tmpArr = new char[arr.Length - 1];
+            int index = 0;
+            bool containsVowels = false;
+            for (int i = arr.Length - 1; i >= 0 && !containsVowels; i--)
+            {
+                if (IsVowel(arr[i]))
+                {
+                    index = i;
+                    containsVowels = true;
+                }
+            }
+            
+            if (containsVowels)
+            {
+                Array.Copy(arr, 0, tmpArr, 0, index);
+                Array.Copy(arr, index + 1, tmpArr, index, arr.Length - index - 1);
+                Array.Clear(arr, 0, arr.Length);
+                tmpArr.CopyTo(arr, 0);
+                tmpArr = null;
+                Console.WriteLine("Была удалена гласная буква\n");
+            }
+            else
+                Console.WriteLine("Гласных букв нет!\n");
+        }
+        static void MenuForCreating(ref char[] arr)
+        {
+            bool exit = false;
+            do
+            {
+                Console.WriteLine("1. Сформировать массив вручную\n2. Сформировать массив с помощью ДСЧ\n3. Назад\n");
+                
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
+                            CreateCharArrManually(ref arr);
+                            Console.WriteLine("Массив сформирован\n");
+                            break;
+                        }
+                    case "2":
+                        {
+                            CreateCharArrWithRandom(ref arr);
+                            Console.WriteLine("Массив сформирован\n");
+                            break;
+                        }
+                    case "3":
+                        {
+                            exit = true;
+                            break;
+                        }
+                    default:
+                        Console.WriteLine("Ошибка ввода\n");
+                        break;
+                }
+            } while (!exit);            
+        }        
+        static void Menu()
+        {
+            bool exit = false;
+            char[] arr = null;
+            int len = 0;
+            do
+            {
+                Console.WriteLine("1. Сформировать массив символов\n2. Удалить последнюю гласную\n3. Печать\n4. Выход из программы");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
+                            len = InsertVariables("Введите размер массива ");
+                            arr = new char[len];
+                            MenuForCreating(ref arr);                            
+                            break;
+                        }
+                    case "2":
+                        {
+                            if (arr != null)
+                            {
+                                RemoveLastVowelFrom(ref arr);
+                            }                                
+                            else
+                                Console.WriteLine("Массив пуст\n");
+                            break;
+                        }                        
+                    case "3":
+                        {
+                            if (arr != null)
+                            {
+                                if (arr[0] != '\0')
+                                    PrintCharArr(arr);
+                                else
+                                    Console.WriteLine("Массив пуст\n");
+                            }                                
+                            else
+                                Console.WriteLine("Массив пуст\n");
+                            break;
+                        }
+                    case "4": exit = true; break;
+                    default:
+                        Console.WriteLine("Ошибка ввода\n");
+                        break;
+                }
+            } while (!exit);
+        }
+        static void Main(string[] args)
+        {
+            Menu();        
+        }
+        static void PrintCharArr(char[] arr)
+        {
+            foreach (char item in arr)
+            {
+                Console.Write($"{item}");
+            }
+            Console.WriteLine();
+        }     
+        static int InsertVariables(string toAsk)
+        {
+            int x;
+            bool isParsed = false;
+            do
+            {
+                Console.WriteLine(toAsk);
+                if (int.TryParse(Console.ReadLine(), out x))
+                    if (x > 0)
+                        isParsed = true;
+                    else
+                        continue;
+                else
+                {
+                    Console.WriteLine("Ошибка ввода! Попробуйте ввести заново!");
+                    continue;
+                }
+            } while (!isParsed);
+
+            return x;
+        }
+    }
+}
---
+++ 3rd semester/OOP/Lab6/SecondEx/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SecondEx
+{
+    class Program
+    {
+        static char[][] CreateJaggedCharArr(string[] strArr)
+        {
+            char[][] jagArr = new char[strArr.Length][];
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                jagArr[i] = strArr[i].ToCharArray();
+            }
+            return jagArr;
+        }
+        static void ReverseElements(ref char[][] jagArr)
+        {
+            for (int i = 0; i < jagArr.Length; i++)
+            {
+                if (jagArr[i].Length == (i + 1) && jagArr.Length != 1)
+                {
+                    Array.Reverse(jagArr[i]);
+                }
+            }
+        }
+        static string[] ConvertToString(char[][] jagArr)
+        {
+            string[] strArr = new string[jagArr.Length];
+            for (int i = 0; i < jagArr.Length; i++)
+            {
+                string tmp = new string(jagArr[i]);
+                strArr[i] = tmp;
+            }
+            return strArr;
+        }
+        static void RevWordsWithNumCorrWithLen(ref string[] strArr)
+        {
+            char[][] jagArr = CreateJaggedCharArr(strArr);            
+            ReverseElements(ref jagArr);
+            strArr = ConvertToString(jagArr);            
+        }
+        static string[] SplitMyString(string inp)
+        {
+            string[] separators = { " ", ",", ", ", ".", ". ", "!", "! ", "—", "–", "?", ";", ":" };
+            string[] strArr = inp.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            return strArr;
+        }
+        static string Menu()
+        {
+            bool toExit = false;
+            string res = "";
+            do
+            {                
+                Console.WriteLine("1. Ввести строку самостоятельно\n" +
+                            "2. Использовать заготовленную строку\n" +
+                            "3. Функции\n" + "4. Выход из программы\n");
+                switch (Console.ReadLine())
+                {
+                    case "1": 
+                        { 
+                            Console.WriteLine("Введите строку"); 
+                            res = Console.ReadLine(); 
+                            break; 
+                        }
+                    case "2":
+                        {
+                            res = "Не поклонясь до избы, и грибов не поднимешь. Когда поднимается одна пылинка, в ней содержится вся земля, когда распускается один цветок, раскрывается целый мир";
+                            Console.WriteLine("Введённая строка: " + res + "\n");
+                            break;
+                        }                                                                   
+                    case "3": toExit = true; break;
+                    case "4":
+                        {
+                            toExit = true;
+                            res = "4";
+                            break;
+                        }
+                    default:
+                        Console.WriteLine("Ошибка выбора!");
+                        break;
+                }
+            } while (!toExit);
+            return res;
+        }
+        static void Task(string inp)
+        {
+            bool toExit = false;
+            string[] strArr = null;
+            do
+            {
+                Console.WriteLine("1. Разбить строку на слова в массив строк\n" +
+                            "2. Перевернуть каждое слово, номер которого совпадает с его длиной\n" +
+                            "3. Печать массива строк\n" +
+                            "4. Печать строки\n" +
+                            "5. Назад\n");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
+                            if (inp != "")
+                            {
+                                strArr = SplitMyString(inp);                                
+                                Console.WriteLine("Строка успешно разбита по словам в массиве строк!\n");
+                            }
+                            else
+                                Console.WriteLine("Строка пустая!\n");
+                            break;
+                        }
+                    case "2":
+                        {
+                            if (strArr != null)
+                            {
+                                RevWordsWithNumCorrWithLen(ref strArr);
+                                Console.WriteLine("Слова в массиве строк были перевёрнуты\n");
+                            }       
+                            else
+                                Console.WriteLine("Массив строк пуст!\n");
+                            break;
+                        }
+                    case "3":
+                        {
+                            if (strArr != null)
+                                PrintStrArr(strArr);
+                            else
+                                Console.WriteLine("Массив строк пуст!\n");
+                            break;
+                        }
+                    case "4":
+                        {
+                            if (strArr != null)
+                            {
+                                string res = string.Join(" ", strArr);
+                                Console.WriteLine(res + "\n");
+                            }
+                            else
+                                Console.WriteLine("Массив строк пуст!\n");
+                            break;
+                        }
+                    case "5": toExit = true; break;
+                    default:
+                        Console.WriteLine("Ошибка выбора!");
+                        break;
+                }
+            } while (!toExit);
+        }
+        static void Task2()
+        {
+            bool exit = false;
+            do
+            {
+                string inp = Menu();
+                if (inp != "4")
+                    Task(inp);
+                else
+                    exit = true;
+            } while (!exit);
+           
+        }
+        static void Main(string[] args)
+        {
+            Task2();            
+        }
+        static void PrintStrArr(string[] strArr)
+        {
+            int count = 1;
+            foreach (string item in strArr)
+            {
+                Console.WriteLine($"{count}) {item}  \tlen = {item.Length}");
+                count++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
