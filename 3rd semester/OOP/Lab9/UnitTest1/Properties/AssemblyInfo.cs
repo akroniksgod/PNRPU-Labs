3edf283224003640e506145d3e042ce22c5e28f8
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2022-01-10 01:06:24 +0500

    Add files via upload

--- 3rd semester/OOP/Lab9/UnitTest1/Properties/AssemblyInfo.cs
+++ 3rd semester/OOP/Lab9/UnitTest1/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTest3")]
+[assembly: AssemblyTitle("UnitTest1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTest3")]
+[assembly: AssemblyProduct("UnitTest1")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("1c15969b-34ef-4286-aabf-4349e7cb5b82")]
+[assembly: Guid("b9bf295a-426e-4f8d-9319-7b3d27e8b29e")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- 3rd semester/OOP/Lab9/UnitTest1/UnitTest1.cs
+++ 3rd semester/OOP/Lab9/UnitTest1/UnitTest1.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Part3;
+using Lab9;
 
-namespace UnitTest3
+namespace UnitTest1
 {
     [TestClass]
     public class UnitTest1
@@ -21,44 +21,21 @@ namespace UnitTest3
             //2 theese should NOT intersect each other
             Diapason d3 = new Diapason(4, 8);
             Diapason d4 = new Diapason(9, 12);
-            Assert.IsFalse(Diapason.Intersects(d4, d3));
-            Assert.IsTrue(d4 > d3);
+            Assert.IsFalse(d4.Intersect(d3));
 
             //3 theese should intersect each other
             Diapason d5 = new Diapason(4, 8);
             Diapason d6 = new Diapason(8, 12.7);
-            Assert.IsTrue(d5 < d6);
+            Assert.IsTrue(d5.Intersect(d6));
 
-            Assert.AreEqual(false, d1 == d4);
-            Assert.AreEqual(true, d2 != d3);
+            //4 theese should intersect each other
+            Diapason d7 = new Diapason(4, 8);
+            Diapason d8 = new Diapason(1, 4);
+            Assert.IsTrue(Diapason.Intersects(d7, d8));
 
-            Assert.AreEqual(4, !d1);
-            Assert.AreEqual(9, (double)d2);
+            Assert.AreEqual(9, d8.Count);
 
-            Diapason toCheck = d3 - 1;
-            Diapason toCompare1 = new Diapason(4, 7);
-            Assert.AreEqual(toCompare1, toCheck);
-
-            Diapason toCheck2 = 1 - d4;
-            Diapason toCompare2 = new Diapason(8, 12);
-            Assert.AreEqual(toCheck2, toCompare2);
-            Assert.AreEqual(11, d6.Count);
-
-            DiapasonArray diapasons1 = new DiapasonArray(5);
-            Assert.AreEqual(new DiapasonArray(5).Size, diapasons1.Size);
-            DiapasonArray diapasons2 = new DiapasonArray(2);
-            diapasons2[0] = d3;
-            diapasons2[1] = d4;
-
-            bool checkRelation = diapasons2[0] != diapasons2[1];
-            Assert.IsTrue(checkRelation);
-            checkRelation = diapasons2[0] == diapasons2[1];
-            Assert.IsFalse(checkRelation);
-
-            DiapasonArray diapasons3 = new DiapasonArray();
-            Assert.AreEqual(4, diapasons3.Count);
-            
-            diapasons2.Write();
+            d8.Show();
         }
     }
 }
