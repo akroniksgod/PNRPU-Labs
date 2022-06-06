72469e7c505e24b674ec5b63e7b87732fd216f37
akroniksgod <73337098+akroniksgod@users.noreply.github.com>
2022-06-07 00:59:15 +0500

    Added person hierarchy

---
+++ 4th semester/OOP/Lab13/PersonLibrary/Id.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLibrary
+{
+    public class Id
+    {
+        public int _number;
+
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+        public Id()
+        {
+            Number = 0;
+        }
+        public Id(int n)
+        {
+            _number = n;
+        }        
+        public override string ToString()
+        {
+            return _number.ToString();
+        }
+    }
+}
