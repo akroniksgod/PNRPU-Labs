dceb27ea1d9696dc7ee79e4923e3f38e75653f6b
tedeev <tedeev@ursalab.ru>
2023-01-13 00:49:38 +0500

    Added Lab15; Removed unnecessary html files.

---
+++ 4th semester/OOP/Lab15/WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace WindowsFormsApp
+{
+    partial class Lab15App
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.GraphicTimer = new System.Windows.Forms.Timer(this.components);
+            this.Graphic = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.PointsLog = new System.Windows.Forms.Label();
+            this.buttonInputDiapason = new System.Windows.Forms.Button();
+            this.textBoxLeftBorder = new System.Windows.Forms.TextBox();
+            this.textBoxRightBorder = new System.Windows.Forms.TextBox();
+            this.labelBorders = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Graphic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GraphicTimer
+            // 
+            this.GraphicTimer.Enabled = true;
+            this.GraphicTimer.Tick += new System.EventHandler(this.GraphicTimer_Tick);
+            // 
+            // Graphic
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Graphic.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Graphic.Legends.Add(legend1);
+            this.Graphic.Location = new System.Drawing.Point(12, 12);
+            this.Graphic.Name = "Graphic";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.Graphic.Series.Add(series1);
+            this.Graphic.Size = new System.Drawing.Size(560, 425);
+            this.Graphic.TabIndex = 0;
+            this.Graphic.Text = "chart1";
+            // 
+            // PointsLog
+            // 
+            this.PointsLog.AutoSize = true;
+            this.PointsLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PointsLog.Location = new System.Drawing.Point(12, 440);
+            this.PointsLog.Name = "PointsLog";
+            this.PointsLog.Size = new System.Drawing.Size(57, 20);
+            this.PointsLog.TabIndex = 1;
+            this.PointsLog.Text = "Точки:";
+            // 
+            // buttonInputDiapason
+            // 
+            this.buttonInputDiapason.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonInputDiapason.Location = new System.Drawing.Point(468, 496);
+            this.buttonInputDiapason.Name = "buttonInputDiapason";
+            this.buttonInputDiapason.Size = new System.Drawing.Size(80, 22);
+            this.buttonInputDiapason.TabIndex = 2;
+            this.buttonInputDiapason.Text = "Рассчёт";
+            this.buttonInputDiapason.UseVisualStyleBackColor = true;
+            this.buttonInputDiapason.Click += new System.EventHandler(this.buttonInputDiapason_Click);
+            // 
+            // textBoxLeftBorder
+            // 
+            this.textBoxLeftBorder.Location = new System.Drawing.Point(325, 496);
+            this.textBoxLeftBorder.Name = "textBoxLeftBorder";
+            this.textBoxLeftBorder.Size = new System.Drawing.Size(50, 20);
+            this.textBoxLeftBorder.TabIndex = 3;
+            // 
+            // textBoxRightBorder
+            // 
+            this.textBoxRightBorder.Location = new System.Drawing.Point(394, 496);
+            this.textBoxRightBorder.Name = "textBoxRightBorder";
+            this.textBoxRightBorder.Size = new System.Drawing.Size(50, 20);
+            this.textBoxRightBorder.TabIndex = 4;
+            // 
+            // labelBorders
+            // 
+            this.labelBorders.AutoSize = true;
+            this.labelBorders.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelBorders.Location = new System.Drawing.Point(322, 461);
+            this.labelBorders.Name = "labelBorders";
+            this.labelBorders.Size = new System.Drawing.Size(186, 16);
+            this.labelBorders.TabIndex = 5;
+            this.labelBorders.Text = "Введите границы рассчёта";
+            // 
+            // Lab15App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.labelBorders);
+            this.Controls.Add(this.textBoxRightBorder);
+            this.Controls.Add(this.textBoxLeftBorder);
+            this.Controls.Add(this.buttonInputDiapason);
+            this.Controls.Add(this.Graphic);
+            this.Controls.Add(this.PointsLog);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Lab15App";
+            this.Text = "Lab15";
+            ((System.ComponentModel.ISupportInitialize)(this.Graphic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer GraphicTimer;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Graphic;
+        private System.Windows.Forms.Label PointsLog;
+        private System.Windows.Forms.Button buttonInputDiapason;
+        private System.Windows.Forms.TextBox textBoxLeftBorder;
+        private System.Windows.Forms.TextBox textBoxRightBorder;
+        private System.Windows.Forms.Label labelBorders;
+    }
+}
+
---
+++ 4th semester/OOP/Lab15/WindowsFormsApp/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Lab15App : Form
+    {
+        private object _locker = new object();
+        public Random rand = new Random();
+        private List<PointF> _points = new List<PointF>();
+        public float lowerBound, upperBound, _x, _y;
+        public Lab15App()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonInputDiapason_Click(object sender, EventArgs e)
+        {
+            float leftB, rightB;
+            Graphic.Series[0].Points.Clear();
+            _points.Clear();
+            if (float.TryParse(textBoxLeftBorder.Text, out leftB) && float.TryParse(textBoxRightBorder.Text, out rightB))
+            {
+                if (leftB < rightB)
+                {
+                    labelBorders.Text = "Введите границы рассчёта: Успех";
+                    Thread t1 = new Thread(new ThreadStart(Count));
+                    Thread t2 = new Thread(new ThreadStart(Print));
+                    t1.Name = "CountThread";
+                    t2.Name = "PrintThread";
+                    t1.IsBackground = true;
+                    t2.IsBackground = true;
+
+                    _x = lowerBound = leftB;
+                    upperBound = rightB;
+
+                    t1.Start();
+                    t2.Start();
+
+                    t1.Priority = ThreadPriority.Highest;
+                }
+                else
+                    labelBorders.Text = "Введите границы рассчёта:\nНеправильный диапазон";
+            }
+            else
+                labelBorders.Text = "Введите границы рассчёта: Ошибка";
+        }
+        private void Count()
+        {
+            while (_x <= upperBound)
+            {
+                lock (_locker)
+                {
+                    _x += 0.01f;
+                    _y = 23 * _x * _x - 33;                   
+                }
+                Thread.Sleep(100);
+            }
+        }
+        private void Print()
+        {
+            while (_x <= upperBound)
+            {
+                lock (_locker)
+                {
+                    _points.Add(new PointF(_x, _y));
+
+                    if (_points.Count > 1000)
+                        _points.RemoveAt(0);
+                }
+                Thread.Sleep(500);
+            }
+        }
+        private void GraphicTimer_Tick(object sender, EventArgs e)
+        {
+            Graphic.Series[0].Points.Clear();
+            lock (_locker)
+            {
+                foreach (var point in _points)
+                {
+                    Graphic.Series[0].Points.AddXY(point.X, point.Y);
+                }
+
+                PointsLog.Text = "Точки:\n";
+                for (int i = _points.Count - 6; i < _points.Count; i++)
+                {
+                    if (i >= 0)
+                        PointsLog.Text += $"X={_points[i].X:0.000}, Y={_points[i].Y:0.000}\n";
+                }
+            }
+        }
+    }
+}
